7742473b8fd17d5b8f1b64295a12505bbc44f500
UCDT <adrian.j.ulibarri@gmail.com>
2021-03-23 18:30:28 -0600

    finished 3.1.5

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,17 @@
-window.alert("this is an alert! Javascript is running!");
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+var playerName = window.prompt("What is your robot's name?");
+// note the lack of quotation marks around playerName
+console.log(playerName);
+console.log("This logs a string, good for leaving yourself a message");
+// this will do math and log 20
+console.log(10 + 10);
+//what is this?
+console.log("our robot's name is " + playerName)
+
+
+
+fight();
+
